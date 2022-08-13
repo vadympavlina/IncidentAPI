99967dad994dc71f9500b89156ef354a1c32c48f
Vadym Pavlina <vadim.table.pc@gmail.com>
2022-08-13 11:03:02 +0300

    Create Models

---
+++ Models/Account.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace IncidentAPI.Models
+{
+    public class Account
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public virtual Contact Contact { get; set; }
+    }
+}
---
+++ Models/Incident.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace IncidentAPI.Models
+{
+    public class Incident
+    {
+        [Key]
+        public string IncidentName { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Description { get; set; }
+
+        public virtual Account Account { get; set; }
+    }
+}
