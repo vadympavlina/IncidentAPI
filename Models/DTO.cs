69b5ee1d4db5207210bd4c7e6be825d7f6a6f922
Vadym Pavlina <vadim.table.pc@gmail.com>
2022-08-13 11:03:17 +0300

    Data Transfer Object

---
+++ Models/DTO.cs
@@ -0,0 +1,11 @@
+﻿namespace IncidentAPI.Models
+{
+    public class DTO
+    {
+        public string AccountName { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Description { get; set; }
+    }
+}
