15c4c158f904d19aa5dd7837a2b9067e553c5775
Vadym Pavlina <vadim.table.pc@gmail.com>
2022-08-20 12:59:04 +0300

    Edit Models

--- Models/DTO.cs
+++ Models/DTO.cs
@@ -1,11 +1,18 @@
-﻿namespace IncidentAPI.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace IncidentAPI.Models
 {
     public class DTO
     {
+        [Required]
         public string AccountName { get; set; }
+        [Required]
         public string FirstName { get; set; }
+        [Required]
         public string LastName { get; set; }
+        [Required]
         public string Email { get; set; }
+        [Required]
         public string Description { get; set; }
     }
 }
